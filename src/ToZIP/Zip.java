3bf725b0f24ecd4c9f63867436340e17fdaf0abc
geros-code <tworki@ya.ru>
2017-12-16 21:38:46 +0300

    added methods to zip files and directories

--- src/ToZIP/Zip.java
+++ src/ToZIP/Zip.java
@@ -1,4 +1,79 @@
 package ToZIP;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class Zip {
+    private ZipOutputStream zos;
+
+
+    private Path sourceDir;
+
+
+    public Zip(Path sourceDir) {
+        this.sourceDir = sourceDir;
+    }
+
+
+    public FileVisitResult visitFile(Path file,
+                                     BasicFileAttributes attributes) {
+        try {
+            Path targetFile = sourceDir.relativize(file);
+
+            zos.putNextEntry(new ZipEntry(targetFile.toString()));
+
+            byte[] bytes = Files.readAllBytes(file);
+            zos.write(bytes, 0, bytes.length);
+            zos.closeEntry();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+
+
+    public void main(String[] args) {
+        String dirPath = args[0];
+        Path sourceDir = Paths.get(dirPath);
+
+        try {
+            String zipFileName = dirPath.concat(".zip");
+            zos = new ZipOutputStream(new FileOutputStream(zipFileName));
+
+            Files.walkFileTree(sourceDir, (FileVisitor<? super Path>) new Zip(sourceDir));
+
+            zos.close();
+        } catch (IOException ex) {
+            System.err.println("I/O Error: " + ex);
+        }
+    }
+
+
+
+    private void zipFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            String zipFileName = file.getName().concat(".zip");
+            FileOutputStream fos = new FileOutputStream(zipFileName);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            zos.putNextEntry(new ZipEntry(file.getName()));
+
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            zos.write(bytes, 0, bytes.length);
+            zos.closeEntry();
+            zos.close();
+
+        } catch (FileNotFoundException ex) {
+            System.err.format("The file %s does not exist", filePath);
+        } catch (IOException ex) {
+            System.err.println("I/O error: " + ex);
+        }
+    }
 }
